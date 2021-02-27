7d4544a66c39e65f668ca663dd6ce7b4c007bde4
osxgirl <osxgirl@mac.com>
2021-02-27 12:42:22 -0500

    Done.

--- index.js
+++ index.js
@@ -46,3 +46,14 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+const [ Bessie, , Dolly, Babe, Little ] = farmAnimals.split(' ')
+const [ black_and_white, , black, pink, ] = farmAnimals.split(' ')
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const [r, o, y, g, b, , v] = colors
+const [, , , , , indg, ] = colors 
+const {name, color, song, job, partner} = muppet
+const {song_2, song_4} = k_muppet.k_album.the_muppet_movie
+const {k_job, k_partner} = k_muppet
